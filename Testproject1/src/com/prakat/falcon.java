9783d3c95b6eca3648d40da9449a387801485c3b
krishnamenan <menanmavericks@gmail.com>
2019-03-12 14:43:21 +0530

    initial commit

---
+++ Testproject1/src/com/prakat/falcon.java
@@ -0,0 +1,11 @@
+package com.prakat;
+
+public class falcon {
+	public static void main(String[] a) {
+		// TODO Auto-generated method stub
+		System.out.println("Falcon Heavy with massive payloads");
+	
+
+	}
+
+}
